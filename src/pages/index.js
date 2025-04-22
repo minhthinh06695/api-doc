27a8cd63db28c2132593ae6db9172480a5d216a6
THINHBM <minhthinh.k29buh@gmail.com>
2025-04-22 16:07:38 +0700

    feat: redirect homepage to /docs/intro
    - Added a redirect in src/pages/index.js to automatically navigate users to /docs/intro when accessing the homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,11 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
+import { Redirect } from '@docusaurus/router';
+
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -29,15 +31,6 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  const { siteConfig } = useDocusaurusContext();
+  return <Redirect to="/docs/intro" />;
 }
